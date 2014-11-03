cd4a12238943fde3ad7b05769cf877d8b1c122c5
Bleso Chung <bleso@icloud.com>
2014-11-03 14:46:09 +0800

    init base files.

---
+++ application/controllers/Site/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bleso
+ * Date: 14/11/3
+ * Time: 下午2:45
+ */
+
+class indexAction extends Yaf_Action_Abstract {
+    public function execute() {
+
+    }
+} 
---
+++ application/views/site/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bleso
+ * Date: 14/11/3
+ * Time: 下午2:45
+ */
+
+echo 'index';
